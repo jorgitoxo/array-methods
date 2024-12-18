1a031b0d9067d3bd08d4c4e18fca38eefb90ed89
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-18 21:12:11 +1300

    Set up javascript and test files

---
+++ filterRange/filterRange.js
@@ -0,0 +1,8 @@
+const filterRange = function (arr, num1, num2) {
+
+}
+
+
+
+// Do not edit below this line
+module.exports = filterRange;
---
+++ filterRange/filterRange.spec.js
@@ -0,0 +1,45 @@
+const filterRange = require('./main')
+
+// describe("filterRange", function() {
+
+//     it("returns the filtered values", function() {
+  
+//       let arr = [5, 3, 8, 1];
+  
+//       let filtered = filterRange(arr, 1, 4); 
+  
+//       assert.deepEqual(filtered, [3, 1]);
+//     });
+  
+//     it("doesn't change the array", function() {
+  
+//       let arr = [5, 3, 8, 1];
+  
+//       let filtered = filterRange(arr, 1, 4); 
+  
+//       assert.deepEqual(arr, [5,3,8,1]);
+//     });
+  
+//   });
+
+  describe("filterRange", function() {
+
+    it("returns the filtered values", function() {
+  
+      let arr = [5, 3, 8, 1];
+  
+      let filtered = filterRange(arr, 1, 4); 
+  
+      expect(filtered).toEqual([3, 1]);
+    });
+  
+    it("doesn't change the array", function() {
+  
+      let arr = [5, 3, 8, 1];
+  
+      let filtered = filterRange(arr, 1, 4); 
+  
+      expect(arr).toEqual([5,3,8,1]);
+    });
+  
+  });
