63c841b8196ebb4ea1d04ff48ba8e716065e808a
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-19 02:28:40 +1300

    Code refactor
    Added comment on reduceRight initial value

--- filterRangeInPlace/filterRangeInPlace.js
+++ filterRangeInPlace/filterRangeInPlace.js
@@ -2,9 +2,11 @@
 // Returns array containning filtered results
 const filterRangeInPlace = function (arr, num1, num2) {
     arr.reduceRight((_, item, index) => {
-            if ((item < num1) || (item > num2))
-                arr.splice(index, 1)
+        if ((item < num1) || (item > num2))
+            arr.splice(index, 1)
     }, null);
+    // initial value (null) is supplied to reduceRight, otherwise
+    // the last element in the array will be used and skipped
 }
 
 // Do not edit below this line
