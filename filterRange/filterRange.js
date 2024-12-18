772bb44837cf4d863b5b111f28c40928e24b527d
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-18 22:41:46 +1300

    Return filtered result
    Added logic to filterRange function
    Changed name of package json file

--- filterRange/filterRange.js
+++ filterRange/filterRange.js
@@ -1,5 +1,7 @@
+// Looks for elements with values higher or equal to a and lower or equal to b in arr
+// Returns array containning filtered results
 const filterRange = function (arr, num1, num2) {
-
+    return arr.filter((item) => ((item >= num1) && (item <= num2)));
 }
 
 
