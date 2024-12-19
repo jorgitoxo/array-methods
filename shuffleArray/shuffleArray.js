ac9ca08fa225a6a353ba18188f326e5a4c350e26
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-20 04:30:46 +1300

    Shuffle array order
    Generated a random index number
    Used object destructuring to swap elements

---
+++ shuffleArray/shuffleArray.js
@@ -0,0 +1,17 @@
+const shuffleArray = function (arr) {
+    arr.map((num, index) => {
+        let randomIndex = Math.floor(Math.random() * index);
+
+        [arr[index], arr[randomIndex]] = [
+        arr[randomIndex], arr[index]];
+    })
+}
+
+// module.exports = shuffleArray;
+
+// Debug console testing
+let arr = [1, 2, 3];
+console.log(arr);
+
+shuffleArray(arr);
+console.log(arr);
