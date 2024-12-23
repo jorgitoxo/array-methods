e40bd37516594b456adf9369f03aaafe8fa24dfe
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-24 02:25:44 +1300

    Group users by id

--- groupByID/groupById.js
+++ groupByID/groupById.js
@@ -1,6 +1,12 @@
 // Creates an object from arr, with id as the key, and array items as values.
 const groupById = function(arr) {
+    let group = {};
 
+    arr.reduce((_, user) =>
+        group[user.id] = user
+    , {});
+
+    return group;
 }
 
 // Jest
